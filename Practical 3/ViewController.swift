4ea502459a44dc73d053ff14fc580b47317af066
Dave Huan <davehuan123@gmail.com>
2019-10-29 15:46:00 +0800

    Lesson 1 Practical 3 Commit

--- Practical 3/ViewController.swift
+++ Practical 3/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func onClick(_ sender: AnyObject) {
+        myLabel.text = "I'm Clicked !"
+    }
 
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
 }
 
